f0e4bcc3f147983715d0dd7181a16f4cbd4eafea
Alejandro-Fierro <89487911+Alejandro-Fierro@users.noreply.github.com>
2021-12-06 14:11:06 -0800

    Update Ejercicio_21.s

--- Ejercicio_21.s
+++ Ejercicio_21.s
@@ -1,5 +1,21 @@
-;comentarios
-.global _start
-_start:
-  mov r4,#100
-  mov r5,#2
+.data
+.text
+.global main
+main:
+  push{r4,r5,r6,lr}
+  mov r4, #100
+  ldr r5,L5
+.L2:
+  mov r1,r4
+  mov r0,r5
+  bl printf
+  sub r4,r4 #2
+  bne .L2
+  mov r0,r4
+  pop {r4,r5,r6,pc}
+.L6:
+  .align 2
+.L5:
+  .word .LC0
+.LC0:
+  .ascii "%d \012\000"
